79c634c03d83590f6c7eb22795fd91cf045d0d24
shoaib <sho.akh99941@gmail.com>
2023-07-07 20:02:41 +0530

    added module css

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,8 @@ const heading ={
 }
   return (
     <div id="main">
-      <h1 >Success</h1>
-      <h1 id='error' >Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      <h1 id='error' className={styles.error}>Error</h1>
     </div>
   )
 }
